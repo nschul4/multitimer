193558e9dfbb80a7a2b35ec2e9f86e578e10a75d
Neal Schultz <neal@collegenet.com>
2020-11-13 07:50:46 -0800

    design

--- app.js
+++ app.js
@@ -4,29 +4,38 @@ angular.module('myApp', [])
 
     .controller("myCtrl", ['$scope', '$interval', function ($scope, $interval) {
 
+        var zeroDate = createZeroDate();
+        $scope.zeroDate = zeroDate;
+
         var timerArr = [
             {
                 name: "mudville",
-                t: new Date(0),
+                tInit: createDefaultDate(),
+                t: createDefaultDate(),
                 going: true,
             },
             {
                 name: "the main road",
-                t: new Date(0),
-            },
-            {
-                name: "shamus",
+                tInit: createDefaultDate(),
+                t: createDefaultDate(),
             },
         ];
         $scope.timerArr = timerArr;
 
         $scope.createTimer = function () {
-            timerArr.push({});
+            timerArr.push({
+                name: "",
+                tInit: createDefaultDate(),
+                t: createDefaultDate(),
+                going: false,
+            });
         }
 
         $scope.remove = function (index) {
-            console.log(index);
-            if (confirm("TODO: present confirmation dialog")) {
+            if (confirm("Confirm:\n    remove timer \""
+                + timerArr[index].name
+                + "\"\n        ?")
+            ) {
                 timerArr.splice(index, 1);
             }
         }
@@ -34,14 +43,30 @@ angular.module('myApp', [])
         $scope.stop = function (timer) {
             timer.going = false;
         }
+
         $scope.start = function (timer) {
+            if (timer.tInit) {
+                if (!hasTimeLeft(timer.t)) {
+                    timer.t = new Date(timer.tInit);
+                }
+            } else {
+                timer.t = createDefaultDate();
+            }
             timer.going = true;
         }
 
-        $scope.addHours = function (timer, ms) {
-            if (timer.t) {
-                var tmp = new Date(timer.t);
-                timer.t.setTime(tmp.getTime() + ms);
+        $scope.reset = function (timer) {
+            if (timer.tInit) {
+                timer.t = new Date(timer.tInit);
+            } else {
+                timer.t = createDefaultDate();
+            }
+        }
+
+        $scope.addHours = function (t, ms) {
+            if (t) {
+                var tmp = new Date(t);
+                t.setTime(tmp.getTime() + ms);
             }
         }
 
@@ -51,9 +76,29 @@ angular.module('myApp', [])
                 var tmp = new Date(timer.t);
                 if (timer.t && timer.going) {
                     timer.t.setTime(tmp.getTime() - refreshInterval);
-                    tmp.getMilliseconds();
-                    tmp.getSeconds();
+                    if (!hasTimeLeft(timer.t)) {
+                        timer.going = false;
+                        timer.t = new Date(zeroDate);
+                    }
                 }
             });
         }, refreshInterval);
+
+        function createZeroDate() {
+            var tmp = new Date(0);
+            tmp.setHours(-24);
+            return tmp;
+
+        }
+
+        function createDefaultDate() {
+            var tmp = createZeroDate();
+            // tmp.setMinutes(10);
+            tmp.setSeconds(1);
+            return tmp;
+        }
+
+        function hasTimeLeft(t) {
+            return t.getTime() > zeroDate.getTime();
+        }
     }]);
