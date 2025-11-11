6e9e65f1975f0068a88b6bcfe04e5ab492bcf3da
Neal Schultz <nschul4@github>
2025-11-11 19:11:41 +0000

    create common lib script shared by multiple bash scripts

--- output_file_list_for_prompt.sh
+++ output_file_list_for_prompt.sh
@@ -1,22 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-output_md() {
-  local file=$1
-  local lang=$2
-  echo "# $file"
-  echo
-  echo -n '```'
-  echo $lang
-  cat "$file"
-  echo
-  echo '```'
-  echo
-}
-
 pushd . > /dev/null
 cd /cygdrive/e/dev/multitimer
 trap 'popd > /dev/null' EXIT
+
+export MYSCRIPTS_LIB=~/dev/bash/sh/lib
+source "$MYSCRIPTS_LIB/lib1.sh"
 
 output_md "index.html" "html"
 output_md "app.js" "javascript"
